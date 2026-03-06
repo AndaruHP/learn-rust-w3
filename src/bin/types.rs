13c6db39ef218e44de56d45d7157f8f3b0a77888
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2026-03-06 15:57:58 +0700

    types

---
+++ src/bin/types.rs
@@ -0,0 +1,65 @@
+#![allow(unused)]
+
+fn main() {
+    // signed integers
+    // range -(2^(n-1) to 2^(n-1) - 1
+    // -(2^(8-1) to 2^(8-1) - 1
+    let i0: i8 = -1;
+    // -(2^(16-1) to 2^(16-1) - 1
+    let i1: i16 = -1;
+    // -(2^(32-1) to 2^(32-1) - 1
+    let i2: i32 = -1;
+    // etc
+
+    // unsigned integers
+    // range 0 to 2^n - 1
+    // 2^8 - 1
+    let i3: u8 = 1;
+
+    // depends on computer architecture
+    let i4: isize = -1;
+    let i5: usize = 1;
+
+    // float
+    let f0: f32 = 0.01;
+    let f1: f64 = 0.01;
+
+    // boolean
+    let b: bool = false;
+
+    // character
+    let c: char = 'c';
+
+    // type conversion
+    let i: i32 = -1;
+    let j: u32 = i as u32;
+
+    // min max
+    let i_max: i32 = i32::MAX;
+    let u_min: i32 = i32::MIN;
+
+    // overflow
+    let mut x = u32::MAX;
+    // x += 1; // it will return 1
+    // u32::checked_add -> return None on overflow
+    let x = u32::checked_add(u32::MAX, 1);
+    println!("checked_add: {:?}", x); // None
+    // u32::wrapping_add -> explicitly allow overflow
+    let x = u32::wrapping_add(u32::MAX, 1);
+    println!("checked_add: {:?}", x); // 1
+
+    // tuple
+    let t: (bool, char, u32) = (true, 'c', u32::MAX);
+    println!("{} {} {}", t.0, t.1, t.2);
+
+    let t = (); // empty tuple = unit type
+    let nested = (('a', 1.23), (true, 1u32, 90i32), ());
+    println!("{}", nested.0.0);
+
+    // destructuring a tuple
+    let t: (bool, char, u32) = (true, 'c', u32::MAX);
+    let (a, b, c) = t;
+    println!("a = {}, b = {}, c = {}", a, b, c);
+    let (_, z, _) = t;
+    println!("z = {}", z);
+}
